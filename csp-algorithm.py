951c30035183393e39b91286e50bd91690f5a50f
Serhii Fedusov <serhii.fedusov@depositphotos.com>
2020-10-23 22:02:45 +0300

    implemented schedule generation as csp algorithm

---
+++ csp-algorithm.py
@@ -0,0 +1,42 @@
+from model import *
+import names
+import random
+
+DISCIPLINES = ["Intro to Computer Science", "Databases", "Linear Algebra", "Calculus", "Algorithms", "System Theory"]
+TEACHERS = ["John Doe", "Ed Sheeran", "LeBron James", "Donald Trump", "Petro Poroshenko"]
+STUDENTS = list(dict.fromkeys([names.get_full_name() for _ in list(range(120))]))
+ROOMS = [Room(110, 10), Room(303, 50), Room(113, 30), Room(404, 50), Room(119, 30), Room(313, 60)]
+
+
+def generate_discipline():
+    lesson_type = random.choice(["lecture", "practice"])
+    teacher = random.choice(TEACHERS)
+    name = random.choice(DISCIPLINES)
+    students = random.sample(STUDENTS, random.randrange(10, 40))
+
+    return Discipline(lesson_type, name, teacher, students, None, None, None)
+
+
+def run_schedule(heuristics):
+    X = [(day, time, room) for day in list(map(lambda x: x, Day)) for time in list(map(lambda x: x, Time)) for room in
+         ROOMS]
+
+    disciplines = [generate_discipline() for _ in range(0, 10)]
+    for i in range(0, len(disciplines)):
+        spot = heuristics(X, disciplines[:i])
+        disciplines[i].day = spot[0]
+        disciplines[i].time = spot[1]
+        disciplines[i].room = spot[2]
+    return Schedule(disciplines)
+
+
+def on_spot(discipline, spot):
+    return discipline.day == spot[0] and discipline.time == spot[1] and discipline.room == spot[2]
+
+
+def mrv(X, disciplines):
+    return sorted(X, key=lambda spot: len(list(filter(lambda disc: on_spot(disc, spot), disciplines))))[0]
+
+
+if __name__ == '__main__':
+    print(run_schedule(mrv))
