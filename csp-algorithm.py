6415c08e2af473e463a330734b78d9bafa7fd852
UndeadBigUnicorn <kravchel16@gmail.com>
2020-10-24 18:37:28 +0300

    update mrv

--- csp-algorithm.py
+++ csp-algorithm.py
@@ -4,12 +4,13 @@ import random
 
 DISCIPLINES = ["Intro to Computer Science", "Databases", "Linear Algebra", "Calculus", "Algorithms", "System Theory"]
 TEACHERS = ["John Doe", "Ed Sheeran", "LeBron James", "Donald Trump", "Petro Poroshenko"]
+LESSON_TYPES = ["lecture", "practice"]
 STUDENTS = list(dict.fromkeys([names.get_full_name() for _ in list(range(120))]))
 ROOMS = [Room(110, 10), Room(303, 50), Room(113, 30), Room(404, 50), Room(119, 30), Room(313, 60)]
 
 
 def generate_discipline():
-    lesson_type = random.choice(["lecture", "practice"])
+    lesson_type = random.choice(LESSON_TYPES)
     teacher = random.choice(TEACHERS)
     name = random.choice(DISCIPLINES)
     students = random.sample(STUDENTS, random.randrange(10, 40))
@@ -18,8 +19,8 @@ def generate_discipline():
 
 
 def run_schedule(heuristics):
-    X = [(day, time, room) for day in list(map(lambda x: x, Day)) for time in list(map(lambda x: x, Time)) for room in
-         ROOMS]
+    X = [(day, time, room, lesson_type) for day in list(map(lambda x: x, Day)) for time in list(map(lambda x: x, Time))
+         for room in ROOMS for lesson_type in LESSON_TYPES]
 
     disciplines = [generate_discipline() for _ in range(0, 10)]
     for i in range(0, len(disciplines)):
@@ -27,11 +28,14 @@ def run_schedule(heuristics):
         disciplines[i].day = spot[0]
         disciplines[i].time = spot[1]
         disciplines[i].room = spot[2]
+        disciplines[i].type = spot[3]
     return Schedule(disciplines)
 
 
 def on_spot(discipline, spot):
-    return discipline.day == spot[0] and discipline.time == spot[1] and discipline.room == spot[2]
+    return (discipline.day == spot[0] and discipline.time == spot[1] and discipline.room == spot[2]) \
+           or (discipline.day == spot[0] and discipline.time == spot[1] and discipline.room == spot[2]
+               and discipline.type != spot[3])
 
 
 def mrv(X, disciplines):
